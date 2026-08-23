24e879303154cc803f4e9d8360eb6030d158365e
traviscomber <n3uralia@gmail.com>
2026-08-23 19:50:27 -0400

    feat: include Banco Central source health

--- src/lib/country-signals/health.ts
+++ src/lib/country-signals/health.ts
@@ -1,3 +1,4 @@
+import { BancoCentralConnector } from "./connectors/banco-central";
 import { DmcWrfConnector } from "./connectors/dmc";
 import { LeyChileConnector } from "./connectors/leychile";
 import { ObservatorioLogisticoConnector } from "./connectors/observatorio-logistico";
@@ -8,11 +9,13 @@ export async function getChileSignalHealth(): Promise<SourceHealth[]> {
   const dmc = new DmcWrfConnector();
   const logistics = new ObservatorioLogisticoConnector();
   const leychile = new LeyChileConnector();
+  const bancoCentral = new BancoCentralConnector();
 
   const activeChecks = new Map<string, Promise<SourceHealth>>([
     [dmc.source.id, dmc.healthCheck()],
     [logistics.source.id, logistics.healthCheck()],
     [leychile.source.id, leychile.healthCheck()],
+    [bancoCentral.source.id, bancoCentral.healthCheck()],
   ]);
 
   return Promise.all(
